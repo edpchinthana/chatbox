1490171134453ea6c6dcc4bc202bb58917137a94
edpchinthana <edpchinthana@outlook.com>
2019-09-10 12:10:49 +0530

    add datetime on messages

--- afterSend.php
+++ afterSend.php
@@ -13,21 +13,22 @@ if($count==0){
 <?php   
         
         include('includes/database.php');
-        $query="SELECT chat.message as 'message', users.name as 'userid',chat.dateTime from users inner join chat on users.id=chat.userId order by dateTime ASC;";
+        $query="SELECT chat.message as 'message', users.name as 'userid',chat.dateTime as 'date' from users inner join chat on users.id=chat.userId order by date ASC;";
         $result=$mysqli->query($query);
         if($result->num_rows>0){
             //Loop through results
             while($row=$result->fetch_assoc()){
                 //Display student info
                 if($row['userid']==$username){
-                    $output  ='<div class="card border-primary float-right col-xs-8 m-2"><h5 class="card-title"><b>';
+                    $output  ='<div class="card border-primary float-right col-xs-8 m-2 px-1 py-1"><h5 class="card-title py-0"><b>';
                 }
                 else{
-                $output  ='<div class="card border-warning float-left col-xs-8 m-2"><h5 class="card-title"><b>';
+                    $output  ='<div class="card border-warning float-left col-xs-8 m-2 px-1 py-1"><h5 class="card-title py-0"><b>';
                 }
                 $output .=$row['userid'];
-                $output .='</b></h5>';
-                $output .= $row['message'].'<br>';
+                $output .='</b></h5><div class="card-body py-0">';
+                $output .= $row['message'].'</div><div class="text-muted text-right">';
+                $output .=$row['date'].'</div>';
                 $output .='</div><br>';
                 $output .='<script>window.scrollBy(0, 100);</script>';     
                 echo $output;
