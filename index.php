a024a0843c78fb93caed2d6049094fd36df1a883
edpchinthana <edpchinthana@outlook.com>
2019-09-10 14:20:37 +0530

    index.php design is done

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<!--Security system-->
+<?php
+ $count=0;
+?>
 
 <!doctype html>
 <html lang="en">
@@ -32,12 +36,189 @@
       <div class="jumbotron">
         <h1 class="text-center">Lets Chat</h1>
         <div class="row">
-            <div class="col-xs-12 col-md-4"></div>
-            <div class="card border-warning col-xs-12 col-md-4">
-                <h3 class="text-center">Please login to chat</h3>
-                <a class="btn btn-warning  w-50 mx-auto " href="login.php">Login</a>
-                <br>
-            </div>
+          <!--Collapse menu-->
+            <div class=" col-xs-12" id="accordion">
+
+              <!--Login collapse-->
+                <div class="card">
+                  <div class="card-header" id="headingOne">
+                    <h3 class="mb-0">
+                      <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        Already member? Click here to Login
+                      </button>
+                    </h3>
+                  </div>
+                  <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordion">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-4"></div>
+                            <div class="card border-warning col-xs-12 col-md-4">
+                            <form role="form" method="post" action="index.php">
+                                <div class="form-group">
+                                    <br>
+                                    <label>Username</label>
+                                    <input name="username" type="username" class="form-control" placeholder="Username">
+                                </div>
+                                <div class="form-group">
+                                    <label>Password</label><br>
+                                    <input name="password" type="password" class="form-control" placeholder="Password">
+                                </div>
+                                <input type="submit" class="btn btn-warning w-25 mx-auto float-right" value="Login"/>
+                                <?php
+                                    include('includes/database.php');
+                                    session_start();
+                                    $username="dasdad";
+                                    $userid="999";
+                                    
+                              if($_POST){
+                                //submitting values
+                                $username =$_POST['username'];
+                                $password =$_POST['password'];
+                                $count=0;
+              
+                            //Create login query
+                                $query="SELECT password,id FROM users where username='$username';"; 
+                            //Get results
+                                $result=$mysqli->query($query);
+                            //Check if the end of the results
+                                if($result->num_rows>0){
+                                  //Loop through results
+                                  while($row = $result->fetch_assoc()){
+                                if($row['password']==$password){
+                                              $count=1;
+                                              $userid=$row['id'];
+                              }
+                            }
+                            if($count==0){
+                              //Printing the error
+                              echo '<br><span style="color:red;">'."<br>Incorrect username or password.".'</span>';	
+                            }else if($count==1){
+                              //If the username and password are matched then proceed to menu.php
+                              $count=3;
+                              header("Location:chatroom.php");
+                              }
+                                }
+                              else{
+                            //Printing the error
+                            echo '<span style="color:red;">'."Incorrect username or password.".'</span>';
+                          }
+                      }
+                  //Passing the count to next page-for security
+                        $_SESSION['varname1']=$count;
+                        $abc=strtolower($username);
+                        $_SESSION['varname2']=$abc;
+                        $_SESSION['varname3']=$userid;
+                ?>    
+                            </form>
+                            
+                            <br>
+                        </div>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+
+                <!--SignUp Collapse-->
+                <div class="card">
+                  <div class="card-header" id="headingTwo">
+                    <h5 class="mb-0">
+                      <button class="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        New member? Click here to Signup
+                      </button>
+                    </h5>
+                  </div>
+                  <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-xs-12 col-md-1"></div>
+                            <div class="card border-warning col-xs-12 col-md-10 ">
+                            <form role="form" method="post" action="index.php">
+                              <br>
+                              <div class="row">
+                                <div class="form-group col-md-6">
+                                  
+                                    <label>First Name</label>
+                                    <input name="firstName" type="text" class="form-control" placeholder="First Name">
+                                </div>
+                                <div class="form-group col-md-6">
+                    
+                                    <label>Last Name</label>
+                                    <input name="lastName" type="Text" class="form-control" placeholder="Last Name">
+                                </div></div>
+                                <div class="form-group">
+                                    
+                                    <label>Email Address</label>
+                                    <input name="email" type="email" class="form-control" placeholder="Email Address">
+                                </div>
+
+                                
+                                <div class="form-group">
+                                    <label>Username</label>
+                                    <div class="row">
+                                    <div class="col-md-6">
+                                    <input name="username" type="username" class="form-control" placeholder="Username">
+                                  </div>
+                                <input type="submit" class="col-md-2 btn btn-success " value="Check"/>
+                              </div></div>
+                              
+                                <div class="form-group">
+                                    <label>Password</label>
+                                    <input name="password" type="password" class="form-control" placeholder="Password">
+                                </div>
+                                <input type="submit" class="btn btn-warning w-25 mx-auto float-right" value="SignUp"/>
+                                <?php                                                                    
+                                    $username="dasdad";
+                                    $userid="999";
+                                    
+                              if($_POST){
+                                //submitting values
+                                $username =$_POST['username'];
+                                $password =$_POST['password'];
+                                $count=0;
+              
+                            //Create login query
+                                $query="SELECT password,id FROM users where username='$username';"; 
+                            //Get results
+                                $result=$mysqli->query($query);
+                            //Check if the end of the results
+                                if($result->num_rows>0){
+                                  //Loop through results
+                                  while($row = $result->fetch_assoc()){
+                                if($row['password']==$password){
+                                              $count=1;
+                                              $userid=$row['id'];
+                              }
+                            }
+                            if($count==0){
+                              //Printing the error
+                              echo '<br><span style="color:red;">'."<br>Incorrect username or password.".'</span>';	
+                            }else if($count==1){
+                              //If the username and password are matched then proceed to menu.php
+                              $count=3;
+                              header("Location:chatroom.php");
+                              }
+                                }
+                              else{
+                            //Printing the error
+                            echo '<span style="color:red;">'."Incorrect username or password.".'</span>';
+                          }
+                      }
+                  //Passing the count to next page-for security
+                        $_SESSION['varname1']=$count;
+                        $abc=strtolower($username);
+                        $_SESSION['varname2']=$abc;
+                        $_SESSION['varname3']=$userid;
+                ?>    
+                            </form>
+                            
+                            <br>
+                        </div>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
         </div>
       </div>
 	<!--Footer-->
